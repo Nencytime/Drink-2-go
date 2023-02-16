4420c9cda73953332e8f2f2b6e3c34ef279cbdda
Дарья <Nencytime.14.92@yandex.ru>
2023-02-17 00:41:15 +0300

    Full project without test

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,5 @@
-/* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
+const navToggle = document.querySelector('.navigation__toggle');
+
+header.classList.remove('no-js');
+navToggle.addEventListener('click', () => header.classList.toggle('is-open'));
---
+++ source/js/slider.js
@@ -0,0 +1,39 @@
+const rangeSliderInit = () => {
+  const range = document.getElementById('range');
+  const inputMin = document.getElementById('min');
+  const inputMax = document.getElementById('max');
+
+  if (!range || !inputMin || !inputMax) return
+
+  const inputs = [inputMin, inputMax];
+
+  noUiSlider.create(range, {
+      start: [0, 900],
+      connect: true,
+      range: {
+        'min': 0,
+        'max': 1000
+      },
+      step: 1,
+    }
+  )
+
+  range.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = parseInt(values[handle]);
+  });
+
+  inputMin.addEventListener('change', function () {
+    range.noUiSlider.set([this.value, null]);
+  });
+
+  inputMax.addEventListener('change', function () {
+    range.noUiSlider.set([null, this.value]);
+  });
+
+}
+
+const init = () => {
+  rangeSliderInit()
+}
+
+window.addEventListener('DOMContentLoaded', init)
