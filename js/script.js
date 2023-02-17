158b910a183f091b4bd32459d2df8b3023971391
Nencytime <99201353+Nencytime@users.noreply.github.com>
2023-02-17 21:36:55 +0000

    Deploying to gh-pages from @ Nencytime/Drink-2-go@d9ffa2fa062889d35b213238181820cb40d8c413 🚀

---
+++ js/script.js
@@ -0,0 +1,1 @@
+const header=document.querySelector(".header"),navToggle=document.querySelector(".navigation__toggle");header.classList.remove("no-js"),navToggle.addEventListener("click",(()=>header.classList.toggle("is-open")));
